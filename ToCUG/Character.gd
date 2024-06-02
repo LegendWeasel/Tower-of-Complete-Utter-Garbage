fee8b3e5b96ea81cb7afcb421e05802dfb5a3881
Weasel <jhoang2002@gmail.com>
2024-06-02 14:27:26 -0700

    Implimented i frames for Characters

--- ToCUG/Character.gd
+++ ToCUG/Character.gd
@@ -11,6 +11,7 @@ var is_invincible = false
 
 # Referance to i-frame timer
 @onready var invincibility_timer = $InvincibilityTimer
+var timer = Timer.new()
 
 var speed: float
 
@@ -22,14 +23,20 @@ func _init(max_health: int, speed: float, attack_power: int,):
 	self.max_health = max_health
 	self.speed = speed
 	self.attack_power = attack_power
+	
+func _ready():
+	#invincibility_timer.connect("timeout", self, "_on_invicibility_timeout")
+	pass
 
+# Moves character
 func move(direction: Vector2, delta: float):
 	velocity = direction.normalized() * speed
-	
+
+# Call when the character is taking damage
 func take_damage(damage: float):
-	
 	# Takes damage only when not in i frames
-	#if current_invincibility_time != invincibiliy_period:
+	if not is_invincible:
+		print(str(self) + " got hit")
 		# Apply damage reduction modifiers
 		# Currently none
 		
@@ -39,11 +46,22 @@ func take_damage(damage: float):
 		# Character dies at 0 health
 		if current_health <= 0:
 			die()
+		
+		# Start the i frames
+		invincibility_timer.start()
+		is_invincible = true
+	else:
+		# Print for debug
+		print(str(self) + " is invincible")
+		
+# Reset invicibility once timer timeouts
+func _on_invincibility_timer_timeout():
+	is_invincible = false
 
 # Deletes the character on death
 # Override for speical death
 func die():
 	pass
-	
+
 func _process(delta):
 	move_and_slide()
